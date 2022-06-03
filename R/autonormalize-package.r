34ca5b09654f8f9a11961e69deee64f3b50fab06
Mark Webster <markwebster204@yahoo.co.uk>
2022-06-03 17:23:26 +0100

    Fix license and package information

---
+++ R/autonormalize-package.r
@@ -0,0 +1,5 @@
+#' @description
+#' This is a port of Alteryx's AutoNormalize Python library
+#' (\url{https://github.com/alteryx/autonormalize}), with various changes /
+#' improvements for the R version.
+"_PACKAGE"
