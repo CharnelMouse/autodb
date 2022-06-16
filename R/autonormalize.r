372989b075e5d2b86d366d5e7e34258bf5ce6b3d
Mark Webster <markwebster204@yahoo.co.uk>
2022-06-16 21:57:43 +0100

    Add dependency filter option to auto_entityset

--- R/autonormalize.r
+++ R/autonormalize.r
@@ -14,6 +14,10 @@ find_dependencies <- function(df, accuracy) {
 #' @param accuracy a numeric in (0, 1], giving the accuracy threshold threshold
 #'   required in order to conclude a dependency.
 #' @param name a character scalar, giving the name of the created entity set.
+#' @param filter a logical, indicating whether to filter the discovered
+#'   functional dependencies before normalising the data.frame. Dependencies
+#'   with any determinant attributes that aren't characters, integers, factors,
+#'   or logicals are removed.
 #'
 #' @return An entity set, containing the data normalised into the required
 #'   number of tables.
@@ -21,9 +25,12 @@ find_dependencies <- function(df, accuracy) {
 auto_entityset <- function(
   df,
   accuracy,
-  name = NA
+  name = NA,
+  filter = FALSE
 ) {
   deps <- find_dependencies(df, accuracy)
   deps <- tuple_relations(deps)
+  if (filter)
+    deps <- filter(deps, df)
   EntitySet(df, deps, name)
 }
