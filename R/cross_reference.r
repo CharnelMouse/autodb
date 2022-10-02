36f1188bd1825ec300072e9ae808f3c241ee0c5b
Mark Webster <markwebster204@yahoo.co.uk>
2022-10-02 13:02:58 +0100

    Replace reference matrix with list of references, keeping relevant attributes to avoid re-calculation

--- R/cross_reference.r
+++ R/cross_reference.r
@@ -21,36 +21,20 @@
 #' @export
 cross_reference <- function(scheme) {
   n_relations <- length(scheme$attrs)
-
   indexes <- lapply(scheme$keys, `[[`, 1)
-  relation_indices <- seq_len(n_relations)
+  references <- calculate_references(indexes, scheme$attrs)
 
-  reference_mat <- calculate_reference_matrix(indexes, scheme$attrs)
   parents <- replicate(n_relations, integer())
   for (n in seq_len(n_relations)) {
-    refs <- reference_mat[n, ]
-    ref_indices <- relation_indices[setdiff(which(refs), n)]
-    parents[[n]] <- ref_indices
+    parents[[n]] <- sort(unique(references$parent[references$child == n]))
   }
 
-  stack <- seq_len(n_relations)
-  relationships <- list()
-  while (length(stack) > 0) {
-    current_relation <- stack[1]
-    child_attrs <- scheme$attrs[[current_relation]]
-    stack <- stack[-1]
-
-    for (parent_index in parents[[current_relation]]) {
-      parent_keys <- scheme$keys[[parent_index]]
-      relationships <- c(
-        relationships,
-        lapply(
-          intersect(child_attrs, unlist(parent_keys)),
-          \(attr) list(c(current_relation, parent_index), attr)
-        )
-      )
-    }
-  }
+  relationships <- Map(
+    \(child, parent, attr) list(c(child, parent), attr),
+    references$child,
+    references$parent,
+    references$attr
+  )
 
   structure(
     list(
@@ -63,22 +47,27 @@ cross_reference <- function(scheme) {
   )
 }
 
-calculate_reference_matrix <- function(indexes, attrs) {
-  ref_detsets <- list()
-  ref_deps <- character()
+calculate_references <- function(indexes, attrs) {
+  # find all links for indexes (should be any candidate key instead)
+  child_ref_attrs <- character()
+  parent_ref_attrs <- character()
   seq_rel <- seq_along(indexes)
-  for (n in seq_rel) {
-    for (m in seq_rel[-n]) {
-      ind <- indexes[[n]]
-      if (all(ind %in% attrs[[m]])) {
-        ref_detsets <- c(ref_detsets, paste(m, ind, sep = "."))
-        ref_deps <- c(ref_deps, paste(n, ind, sep = "."))
+  for (parent in seq_rel) {
+    for (child in seq_rel[-parent]) {
+      parent_index <- indexes[[parent]]
+      if (all(parent_index %in% attrs[[child]])) {
+        child_ref_attrs <- c(child_ref_attrs, paste(child, parent_index, sep = "."))
+        parent_ref_attrs <- c(parent_ref_attrs, paste(parent, parent_index, sep = "."))
       }
     }
   }
 
-  ref_attrs <- unique(c(unlist(ref_detsets), unlist(ref_deps)))
-  ref_vecs <- list(determinant_sets = ref_detsets, dependents = ref_deps) |>
+  # remove extraneous relationships, i.e. those that skip tables in the hierarchy
+  ref_attrs <- unique(c(child_ref_attrs, parent_ref_attrs))
+  ref_vecs <- list(
+    determinant_sets = child_ref_attrs,
+    dependents = parent_ref_attrs
+  ) |>
     convert_to_integer_attributes(ref_attrs) |>
     remove_extraneous_dependencies()
   # convert back to characters
@@ -87,27 +76,16 @@ calculate_reference_matrix <- function(indexes, attrs) {
       as.character() |>
       strsplit(".", fixed = TRUE)
   }
-  filtered_determinant_sets <- convert_back(unlist(ref_vecs$determinant_sets))
-  filtered_dependents <- convert_back(ref_vecs$dependents)
+  filtered_child_ref_attrs <- convert_back(unlist(ref_vecs$determinant_sets))
+  filtered_parent_ref_attrs <- convert_back(ref_vecs$dependents)
   rel_inds <- function(x) strtoi(vapply(x, `[[`, character(1), 1))
   rel_attrs <- function(x) vapply(x, `[[`, character(1), 2)
-  filtered_children <- rel_inds(filtered_determinant_sets)
-  filtered_parents <- rel_inds(filtered_dependents)
-  filtered_attrs <- rel_attrs(filtered_determinant_sets)
-  stopifnot(identical(filtered_attrs, rel_attrs(filtered_dependents)))
+  filtered_attrs <- rel_attrs(filtered_child_ref_attrs)
+  stopifnot(identical(filtered_attrs, rel_attrs(filtered_parent_ref_attrs)))
 
-  unique_ref_splits <- !duplicated(Map(c, filtered_children, filtered_parents))
-  unique_ref_children <- filtered_children[unique_ref_splits]
-  unique_ref_parents <- filtered_parents[unique_ref_splits]
-  ref_order <- order(unique_ref_children, unique_ref_parents)
-  children <- unique_ref_children[ref_order]
-  parents <- unique_ref_parents[ref_order]
-
-  n_tables <- length(indexes)
-  reference_mat <- matrix(FALSE, nrow = n_tables, ncol = n_tables)
-  for (ref in seq_along(ref_order)) {
-    reference_mat[children[ref], parents[ref]] <- TRUE
-  }
-
-  reference_mat
+  list(
+    child = rel_inds(filtered_child_ref_attrs),
+    parent = rel_inds(filtered_parent_ref_attrs),
+    attr = filtered_attrs
+  )
 }
