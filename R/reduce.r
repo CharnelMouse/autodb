9e1a126b8095f082e6dbdc60c80ac64995312a30
Mark Webster <markwebster204@yahoo.co.uk>
2023-02-12 23:19:14 +0000

    Add database schema method for reduction

--- R/reduce.r
+++ R/reduce.r
@@ -1,39 +1,119 @@
-#' Remove database relations not linked to the main relation
+#' Remove relations not linked to the main relations
 #'
-#' Filters a database's relations, keeping only those linked to the main
-#' relation by foreign key references. The main relations is considered to be
-#' the relation with the largest number of records. Foreign key relationships
-#' involving removed relations are also removed.
+#' Filters an object's relations, keeping only the main relations, and those
+#' considered ancestors via foreign key references. Foreign key
+#' relationships involving removed relations are also removed.
 #'
-#' This function is mostly intended for simplifying a database for the purposes
-#' of exploration, particularly by examining plots. While the filtering might
-#' remove important auxiliary relations, it's also likely to remove any based on
-#' spurious dependencies, of which some databases can contain many.
+#' Details on how the main tables are chosen are given in individual methods.
+#'
+#' This function is mostly intended for simplifying a database, or a database
+#' schema, for the purposes of exploration, particularly by examining plots.
+#' While the filtering might remove important auxiliary relations, it's also
+#' likely to remove any based on spurious dependencies, of which some databases
+#' can contain many.
+#'
+#' @param x An object whose relations are to be filtered.
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @return An object of the same class as \code{x}, with the auxiliary relations
+#'   and foreign key relationships removed.
+#' @export
+reduce <- function(x, ...) {
+  UseMethod("reduce", x)
+}
+
+#' Remove database relations not linked to the main relations
+#'
+#' Filters a database's relations, keeping only the main relations, and those
+#' considered ancestors via foreign key references. Foreign
+#' key relationships involving removed relations are also removed.
+#'
+#' The main relations are considered to be the relations with the largest number
+#' of records.
 #'
 #' Using \code{\link{rejoin}} on the database resulting from \code{reduce} is
 #' likely to fail or return incomplete results.
 #'
-#' @param database A database, whose relations are to be filtered.
+#' @param x A database, whose relations are to be filtered.
+#' @inheritParams reduce
 #'
 #' @return A database, with the auxiliary relations and foreign key
 #'   relationships removed.
-#' @export
-reduce <- function(database) {
-  relation_nrows <- vapply(database$relations, \(x) nrow(x$df), integer(1))
+#' @exportS3Method
+reduce.database <- function(x, ...) {
+  relation_nrows <- vapply(x$relations, \(x) nrow(x$df), integer(1))
   queue <- names(relation_nrows)[relation_nrows == max(relation_nrows)]
   kept <- character()
   while (length(queue) > 0) {
     current <- queue[1]
     queue <- queue[-1]
     kept <- union(kept, current)
-    current_parents <- database$relations[[current]]$parents
+    current_parents <- x$relations[[current]]$parents
     queue <- union(queue, setdiff(current_parents, kept))
   }
-  sorted_kept <- kept[order(match(kept, names(database$relations)))]
-  database$relations <- database$relations[sorted_kept]
-  database$relationships <- Filter(
+  sorted_kept <- kept[order(match(kept, names(x$relations)))]
+  x$relations <- x$relations[sorted_kept]
+  x$relationships <- Filter(
     \(r) all(is.element(r[c(1, 3)], sorted_kept)),
-    database$relationships
+    x$relationships
   )
-  database
+  x
+}
+
+#' Remove database schema relations not linked to the given relations
+#'
+#' Filters a database schema's relations, keeping only the given relations, and
+#' those considered ancestors via foreign key references. Foreign key
+#' relationships involving removed relations are also removed.
+#'
+#' This method takes a given set of main relations, rather than inferring them.
+#'
+#' Using \code{\link{rejoin}} on the database resulting from decomposing a data
+#' frame with the reduced schema is likely to fail or return incomplete results.
+#'
+#' @param x A database schema, whose relations are to be filtered.
+#' @param main A character vector, containing names of relations to be
+#'   considered as the "main" relations.
+#' @inheritParams reduce
+#'
+#' @return A database schema, with the auxiliary relations and foreign key
+#'   relationships removed.
+#' @exportS3Method
+reduce.database_schema <- function(x, main, ...) {
+  main_indices <- match(main, x$relation_names)
+  if (anyNA(main_indices))
+    stop(
+      "main contains names for relations not present: ",
+      toString(main[is.na(main_indices)])
+    )
+  queue <- main_indices
+  kept <- integer()
+  while (length(queue) > 0) {
+    current <- queue[1]
+    queue <- queue[-1]
+    kept <- union(kept, current)
+    current_parents <- x$parents[[current]]
+    queue <- union(queue, setdiff(current_parents, kept))
+  }
+  sorted_kept <- sort(kept)
+  x$attrs <- x$attrs[sorted_kept]
+  x$keys <- x$keys[sorted_kept]
+  x$parents <- lapply(x$parents[sorted_kept], \(p) match(p, sorted_kept))
+  x$relation_names <- x$relation_names[sorted_kept]
+  x$relationships <- Filter(
+    \(r) all(is.element(r[[1]], sorted_kept)),
+    x$relationships
+  )
+  x$relationships <- Filter(
+    \(r) is.element(r[[2]], x$relation_names),
+    x$relationships
+  )
+  x$relationships <- lapply(
+    x$relationships,
+    \(r) {
+      r[[1]] <- match(r[[1]], sorted_kept)
+      r
+    }
+  )
+  x
 }
--- tests/testthat/test-reduce.r
+++ tests/testthat/test-reduce.r
@@ -1,6 +1,6 @@
 library(hedgehog)
 
-describe("reduce", {
+describe("reduce.database", {
   it("is idempotent", {
     has_idempotent_reduction <- function(df) {
       database <- autodb(as.data.frame(df), 1, ensure_lossless = FALSE)
@@ -68,5 +68,79 @@ describe("reduce", {
       gen_df(6, 7, minrow = 1L),
       all_non_parents_in_reduction_have_same_nrow
     )
+  })
+})
+
+describe("reduce.database_schema", {
+  it("is idempotent", {
+    has_idempotent_reduction <- function(df) {
+      database_schema <- dfd(as.data.frame(df), 1) |>
+        flatten() |>
+        normalise() |>
+        cross_reference(ensure_lossless = TRUE)
+      once_schema <- reduce(database_schema, database_schema$relation_names[1L])
+      twice_schema <- reduce(once_schema, database_schema$relation_names[1L])
+      expect_identical(twice_schema, once_schema)
+    }
+    forall(gen_df(6, 7), has_idempotent_reduction)
+  })
+  it("removes added relations with less rows than existing non-parent relations", {
+    removes_added_non_parent_with_non_maximum_nrow <- function(df) {
+      database_schema <- dfd(df, 1) |>
+        flatten() |>
+        normalise() |>
+        cross_reference(ensure_lossless = TRUE)
+      once <- reduce(database_schema, database_schema$relation_names[1L])
+      once_plus_small <- once
+      once_plus_small$attrs <- c(once_plus_small$attrs, list("extra_attr"))
+      once_plus_small$keys <- c(once_plus_small$keys, list(list("extra_attr")))
+      once_plus_small$parents <- c(once_plus_small$parents, list(integer()))
+      once_plus_small$relation_names <- c(
+        once_plus_small$relation_names,
+        "extra_table"
+      )
+      once_plus_small$all_attrs <- c(once_plus_small$all_attrs, "extra_attr")
+      twice <- reduce(once_plus_small, database_schema$relation_names[1L])
+      twice_minus_small_attr <- twice
+      twice_minus_small_attr$all_attrs <- setdiff(twice$all_attrs, "extra_attr")
+      expect_identical(twice_minus_small_attr, once)
+    }
+    forall(
+      gen_df(6, 7, minrow = 1L),
+      removes_added_non_parent_with_non_maximum_nrow
+    )
+  })
+  it("returns a subset", {
+    reduced_to_subset <- function(df) {
+      database_schema <- dfd(df, 1) |>
+        flatten() |>
+        normalise() |>
+        cross_reference(ensure_lossless = TRUE)
+      reduced <- reduce(database_schema, database_schema$relation_names[1L])
+      kept <- match(reduced$relation_names, database_schema$relation_names)
+      expect_true(all(!is.na(kept)))
+      expect_true(!anyDuplicated(kept))
+      expect_identical(reduced$attrs, database_schema$attrs[kept])
+      expect_identical(reduced$keys, database_schema$keys[kept])
+      expect_identical(
+        lapply(reduced$parents, \(p) reduced$relation_names[p]),
+        lapply(database_schema$parents[kept], \(p) database_schema$relation_names[p])
+      )
+      expect_identical(
+        lapply(
+          reduced$relationships,
+          \(r) {r[[1]] <- reduced$relation_names[r[[1]]]; r}
+        ),
+        lapply(
+          Filter(
+            \(r) all(r[[1]] %in% kept) && r[[2]] %in% reduced$relation_names,
+            database_schema$relationships
+          ),
+          \(r) {r[[1]] <- database_schema$relation_names[r[[1]]]; r}
+        )
+      )
+      expect_identical(reduced$all_attrs, database_schema$all_attrs)
+    }
+    forall(gen_df(6, 7, minrow = 1L), reduced_to_subset)
   })
 })
