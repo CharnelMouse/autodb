74a3e06306fc34d79764978911accd22851931b8
Mark Webster <markwebster204@yahoo.co.uk>
2023-04-26 23:09:37 +0100

    Speed up new group indices creation in stripped partition product

--- R/dfd.r
+++ R/dfd.r
@@ -793,14 +793,18 @@ check_AD_nocache <- function(df, rhs, lhs_set, partitions, threshold, limit, par
 
 stripped_partition_product <- function(sp1, sp2, n_rows) {
   # vectorised union of stripped partitions to replace algorithm from Tane
+  if (length(sp1) == 0L || length(sp2) == 0L)
+    return(list())
   tab <- rep(NA_integer_, n_rows)
   tab[unlist(sp1)] <- rep(seq_along(sp1), lengths(sp1))
   tab2 <- rep(NA_integer_, n_rows)
   tab2[unlist(sp2)] <- rep(seq_along(sp2), lengths(sp2))
-  tab_both <- rep(NA_integer_, n_rows)
   in_both <- which(!is.na(tab) & !is.na(tab2))
-  tab_both[in_both] <- paste(tab[in_both], tab2[in_both])
-  sp <- split(seq_len(n_rows), tab_both)
+  tab_both <- paste(tab[in_both], tab2[in_both])
+  # not pre-factoring with specified levels results in split calling
+  # order(tab_both), which can be very slow for long tab_both
+  tab_both <- factor(tab_both, levels = unique(tab_both), ordered = FALSE)
+  sp <- split(in_both, tab_both, drop = TRUE)
   unname(sp[lengths(sp) >= 2])
 }
 
