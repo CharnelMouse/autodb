ba250af1973429c0ec3016615004fb68ad7ef107
Mark Webster <markwebster204@yahoo.co.uk>
2025-06-09 11:13:41 +0100

    Extract stripped partition inversion

--- R/partition.r
+++ R/partition.r
@@ -10,10 +10,8 @@ stripped_partition_product <- function(sp1, sp2, n_rows) {
   # vectorised union of stripped partitions to replace algorithm from Tane
   if (length(sp1) == 0L || length(sp2) == 0L)
     return(list())
-  tab <- rep(NA_integer_, n_rows)
-  tab[unlist(sp1)] <- rep(seq_along(sp1), lengths(sp1))
-  tab2 <- rep(NA_integer_, n_rows)
-  tab2[unlist(sp2)] <- rep(seq_along(sp2), lengths(sp2))
+  tab <- invert_partition(sp1, n_rows)
+  tab2 <- invert_partition(sp2, n_rows)
   in_both <- which(!is.na(tab) & !is.na(tab2))
   tab_both <- paste(tab[in_both], tab2[in_both])
   # not pre-factoring with specified levels results in split calling
@@ -54,4 +52,10 @@ value_partition_error <- function(rhs_value_lhs_partition, n_rows) {
     integer(1)
   ))
   n_rows - majorities_total
+}
+
+invert_partition <- function(sp, n_rows) {
+  inv <- rep(NA_integer_, n_rows)
+  inv[unlist(sp)] <- rep(seq_along(sp), lengths(sp))
+  inv
 }
