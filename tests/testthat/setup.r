388bb36ec06a86d90f67534f38f92b0a045a9590
Mark Webster <markwebster204@yahoo.co.uk>
2025-10-10 08:27:27 +0100

    Add partial match warnings to testing
    Thanks to Hadley Wickham for mentioning these.

--- tests/testthat/setup.r
+++ tests/testthat/setup.r
@@ -1,3 +1,8 @@
 library(hedgehog)
+options(
+  warnPartialMatchArgs = TRUE,
+  warnPartialMatchDollar = TRUE,
+  warnPartialMatchAttr = TRUE
+)
 if (!isTRUE(as.logical(Sys.getenv("NOT_CRAN", "false"))))
   options(hedgehog.tests = 20)
