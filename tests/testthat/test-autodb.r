294f88d8ad1991c366b8a3b08628cf2c3d840b6b
Mark Webster <markwebster204@yahoo.co.uk>
2026-06-21 22:53:00 +0100

    Remove redundant autodb test

--- tests/testthat/test-autodb.r
+++ tests/testthat/test-autodb.r
@@ -147,43 +147,6 @@ describe("autodb", {
       list(c("Title", "Format"))
     )
   })
-  it("doesn't choose keys with incorrect types as the index if filter = TRUE", {
-    df <- data.frame(
-      Title = rep(
-        c(
-          "Beginning MySQL Database Design and Optimization",
-          "The Relational Model for Database Management: Version 2"
-        ),
-        each = 2
-      ),
-      Format = c("Hardcover", "E-book", "E-book", "Paperback"),
-      Author = rep(c("Chad Russell", "E.F. Codd"), each = 2),
-      Author_Nationality = rep(c("American", "British"), each = 2),
-      Price = c(4999L, 2234L, 1388L, 3999L),
-      Thickness = "Thick",
-      Genre_ID = rep(1:2, each = 2),
-      Genre_Name = rep(c("Tutorial", "Popular science"), each = 2),
-      Publisher_ID = rep(1:2, each = 2)
-    )
-    database_nonfiltered <- autodb(df)
-    expect_setequal(
-      names(database_nonfiltered),
-      c("Price", "Title", "constants")
-    )
-    expect_identical(
-      keys(database_nonfiltered)$Price,
-      list("Price", c("Title", "Format"))
-    )
-    database_filtered <- autodb(df, exclude_class = "integer")
-    expect_setequal(
-      names(database_filtered),
-      c("Title_Format", "Title", "constants")
-    )
-    expect_identical(
-      keys(database_filtered)$Title_Format,
-      list(c("Title", "Format"))
-    )
-  })
   it("correctly handles attributes with non-df-standard names", {
     df <- data.frame(1:3, c(1, 1, 2), c(1, 2, 2)) |>
       stats::setNames(c("A 1", "B 2", "C 3"))
